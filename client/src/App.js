f88637953d9fd825863e3372594dbdbd067c3373
Ruslan1493 <star93@list.ru>
2022-11-01 16:41:32 +0200

    added styles for navigation part 1

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import './App.css';
+import style from './App.module.scss';
 import { Routes, Route } from 'react-router-dom';
 import Register from './components/User/Register';
 import Home from './components/Home';
-import Navigation from './components/Navigation';
+import Navigation from './components/Navigation/Navigation';
 import NotFound from './components/Notfound';
 import Login from './components/User/Login';
 import CreateProperty from './components/CreateProperty';
@@ -11,18 +11,20 @@ import CreateProperty from './components/CreateProperty';
 
 function App() {
   return (
-    <React.Fragment>
-      {/* <LogProvider> */}
-      <Navigation />
-      {/* </LogProvider> */}
-      <Routes>
-        <Route exact path='/' element={<Home />} />
-        <Route path='/property/create' element={<CreateProperty />} />
-        <Route path='/users/register/' element={<Register />} />
-        <Route path='/users/login/' element={<Login />} />
-        <Route path='*' element={<NotFound />} />
-      </Routes>
-    </React.Fragment>
+    <div className={style.app}>
+      <React.Fragment>
+        {/* <LogProvider> */}
+        <Navigation />
+        {/* </LogProvider> */}
+        <Routes>
+          <Route exact path='/' element={<Home />} />
+          <Route path='/property/create' element={<CreateProperty />} />
+          <Route path='/users/register/' element={<Register />} />
+          <Route path='/users/login/' element={<Login />} />
+          <Route path='*' element={<NotFound />} />
+        </Routes>
+      </React.Fragment>
+    </div>
   );
 }
 
---
+++ client/src/components/Navigation/Navigation.jsx
@@ -0,0 +1,20 @@
+import { Link } from 'react-router-dom';
+import style from './Navigation.module.scss';
+
+const Navigation = () => {
+    return (
+        <>
+            <div className={style.navigation}>
+                <h1>Navigation</h1>
+                <ul className={style.nav_list}>
+                    <Link to='/'>Home</Link>
+                    <Link to='/property/create'>Create property</Link>
+                    <Link to='/users/register'>Register</Link>
+                    <Link to='/users/login'>Login</Link>
+                </ul>
+            </div>
+        </>
+    )
+};
+
+export default Navigation;
